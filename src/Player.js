34e6f96c632473b607699a09d960f4a876429809
Bounor <leobounor@gmail.com>
2023-03-14 18:13:10 +0100

    init login + spotifyAPI + sidebar

---
+++ src/Player.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style/Player.css";
+import Sidebar from "./Sidebar";
+
+function Player({ spotify }) {
+	return (
+		<div className="player">
+			<div className="player__body">
+				<Sidebar />
+			</div>
+		</div>
+	)
+};
+
+export default Player;
